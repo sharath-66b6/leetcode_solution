7917e5f253b2a583b3b13f32f404e268eba894dd
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2024-12-21 16:01:14 +0530

    Time: 104 ms (75.95%), Space: 158.9 MB (98.73%) - LeetHub

--- 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
+++ 3058-maximum-number-of-k-divisible-components/3058-maximum-number-of-k-divisible-components.cpp
@@ -1,29 +1,42 @@
+typedef long long int ll;
+const int N = 3e4+5;
+
+vector<vector<int>> g(N);
+vector<ll> val(N);
+int result;
+int K;
+
+int MaxComponents (int src, int par) {
+    ll leftOver = val[src];
+    
+    for (auto i : g[src]) {
+        if (i == par) continue;
+        leftOver += MaxComponents(i, src);
+    }
+    if (leftOver % K == 0) {
+        result ++;
+        leftOver = 0;
+    }
+    return leftOver;
+}
+
 class Solution {
 public:
-    map<int, vector<int>> adj;
-    vector<int> val;
-    int res = 0, x;
-
-    long long dfs(int cur, int p) {
-        long long total = val[cur];
-        for (auto child : adj[cur]) {
-            if (child != p)
-                total += dfs(child, cur);
-        }
-        if (total % x == 0)
-            res++;
-        return total;
-    }
-
     int maxKDivisibleComponents(int n, vector<vector<int>>& edges, vector<int>& values, int k) {
-        val = values;
-        x = k;
-        for (const auto& edge : edges) {
-            int i = edge[0], j = edge[1];
-            adj[i].push_back(j);
-            adj[j].push_back(i);
+        result = 0;
+        K = k;
+        for (int j = 0; j < n; j ++) {
+            g[j].clear();
+            val[j] = values[j];
         }
-        dfs(0, -1);
-        return res;
+        for (auto e : edges) {
+            g[e[0]].push_back(e[1]);
+            g[e[1]].push_back(e[0]);
+        }
+        
+        ll leftOver = MaxComponents(0, -1);
+        if (leftOver != 0) result ++;
+        
+        return result;
     }
 };
