e91e2edb42112452990fb784b3cf8b3538cb9a66
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2025-03-03 12:11:28 +0530

    Time: 108 ms (28.89%), Space: 186.7 MB (18.58%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,37 +1,74 @@
 class LRUCache {
 private:
+    class node {
+      public:
+        int key;
+        int val;
+        node * next;
+        node * prev;
+        node(int _key, int _val) {
+            key = _key;
+            val = _val;
+        }
+    };
     int n;
-    unordered_map<int, int> mp;
-    unordered_map<int, list<int>::iterator> key_pos;
-    list<int> keys;
+    unordered_map<int, node*> mp;
+    node* head = new node(-1, -1);
+    node* tail = new node(-1, -1);
+
+    void insertNode(node* curr){
+        node* fristnode = head->next;
+        
+        curr->next = fristnode;
+        curr->prev = head;
+        head->next = curr;
+        fristnode->prev = curr;
+    }
+
+    void deleteNode(node* curr){
+        node* prevNode = curr->prev;
+        node* nextNode = curr->next;
+
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+    }
 
 public:
-    LRUCache(int capacity) {
+    LRUCache(int capacity){
         n = capacity;
+        head->next = tail;
+        tail->prev = head;
     }
     
     int get(int k) {
         if (mp.find(k) != mp.end()) {
-            keys.erase(key_pos[k]);
-            keys.push_front(k);
-            key_pos[k] = keys.begin();
-            return mp[k];
+            node* curr = mp[k];
+            int v = curr->val;
+
+            mp.erase(k);
+            deleteNode(curr);
+            insertNode(curr);
+            mp[k] = head->next;
+
+            return v;
         }
         return -1;
     }
     
     void put(int k, int v) {
         if (mp.find(k) != mp.end()) {
-            keys.erase(key_pos[k]);
-        } else if (keys.size() >= n) {
-            int lru_key = keys.back();
-            keys.pop_back();
-            mp.erase(lru_key);
-            key_pos.erase(lru_key);
+            node* curr = mp[k];
+            mp.erase(k);
+            deleteNode(curr);
+        } 
+        if (mp.size() >= n) {
+            node* lastNode = tail->prev;
+            mp.erase(lastNode->key);
+            deleteNode(lastNode);
         }
-
-        keys.push_front(k);
-        mp[k] = v;
-        key_pos[k] = keys.begin();
+        
+        node* curr = new node(k, v);
+        insertNode(curr);
+        mp[k] = head->next;
     }
 };
