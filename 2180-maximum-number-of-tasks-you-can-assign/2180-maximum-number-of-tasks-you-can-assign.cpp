9c23364b62f801af75691d1d983687b9b4c92f24
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2025-05-01 21:03:05 +0530

    Time: 983 ms (14.53%), Space: 286 MB (77.91%) - LeetHub

--- 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
+++ 2180-maximum-number-of-tasks-you-can-assign/2180-maximum-number-of-tasks-you-can-assign.cpp
@@ -1,50 +1,51 @@
 class Solution {
- public:
-  int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
-    int ans = 0;
-    int l = 0;
-    int r = min(tasks.size(), workers.size());
+    public:
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        int ans = 0;
+        int l = 0;
+        int r = min(tasks.size(), workers.size());
 
-    ranges::sort(tasks);
-    ranges::sort(workers);
+        ranges::sort(tasks);
+        ranges::sort(workers);
 
-    auto canComplete = [&](int k, int pillsLeft) {
-      map<int, int> sortedWorkers;
-      for (int i = workers.size() - k; i < workers.size(); ++i)
-        ++sortedWorkers[workers[i]];
+        auto canComplete = [&](int k, int pillsLeft) {
+            map<int, int> sortedWorkers;
+            for (int i = workers.size() - k; i < workers.size(); ++i)
+                ++sortedWorkers[workers[i]];
 
-      for (int i = k - 1; i >= 0; --i) {
-        auto it = sortedWorkers.lower_bound(tasks[i]);
-        if (it != sortedWorkers.end()) {
-          if (--(it->second) == 0)
-            sortedWorkers.erase(it);
-        } else if (pillsLeft > 0) {
-          it = sortedWorkers.lower_bound(tasks[i] - strength);
-          if (it != sortedWorkers.end()) {
-            if (--(it->second) == 0)
-              sortedWorkers.erase(it);
-            --pillsLeft;
-          } else {
+            for (int i = k - 1; i >= 0; --i) {
+                auto it = sortedWorkers.lower_bound(tasks[i]);
+                if (it != sortedWorkers.end()) {
+                if (--(it->second) == 0)
+                    sortedWorkers.erase(it);
+                } else if (pillsLeft > 0) {
+                it = sortedWorkers.lower_bound(tasks[i] - strength);
+                if (it != sortedWorkers.end()) {
+                    if (--(it->second) == 0)
+                    sortedWorkers.erase(it);
+                    --pillsLeft;
+                } else {
+                    return false;
+                }
+            } else {
             return false;
-          }
-        } else {
-          return false;
+            }
         }
-      }
 
-      return true;
-    };
+        return true;
+        };
 
-    while (l <= r) {
-      const int m = (l + r) / 2;
-      if (canComplete(m, pills)) {
-        ans = m;
-        l = m + 1;
-      } else {
-        r = m - 1;
-      }
+        while (l <= r) {
+            const int m = (l + r) / 2;
+            if (canComplete(m, pills)) {
+                ans = m;
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return ans;
     }
-
-    return ans;
-  }
 };
+
