7da285c49fbbb79aa0db4a6a501ad4c3df2260dc
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2025-05-05 19:43:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (72.14%) - LeetHub

--- 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
+++ 0806-domino-and-tromino-tiling/0806-domino-and-tromino-tiling.cpp
@@ -1,15 +1,14 @@
 class Solution {
- public:
-  int numTilings(int n) {
-    constexpr int Mod = 1'000'000'007;
-    vector<long> dp(1001);
-    dp[1] = 1;
-    dp[2] = 2;
-    dp[3] = 5;
-
-    for (int i = 4; i <= n; ++i)
-      dp[i] = (2 * dp[i - 1] + dp[i - 3]) % Mod;
-
-    return dp[n];
-  }
+public:
+    int numTilings(int n) {
+        int mod = 1000*1000*1000+7;
+        vector<long long> dp(1001);
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=5;
+        for(int i=4;i<=n;i++){
+            dp[i] = (2*dp[i-1]+dp[i-3])%mod;
+        }
+        return dp[n];
+    }
 };
