dd628cb383907b071edcf15874b9b7d01f4907df
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2025-05-31 23:27:11 +0530

    Time: 0 ms (100%), Space: 66.7 MB (59.1%) - LeetHub

--- 2145-grid-game/2145-grid-game.cpp
+++ 2145-grid-game/2145-grid-game.cpp
@@ -1,17 +1,17 @@
 class Solution {
- public:
-  long long gridGame(vector<vector<int>>& grid) {
-    const int n = grid[0].size();
-    long ans = LONG_MAX;
-    long sumRow0 = accumulate(grid[0].begin(), grid[0].end(), 0L);
-    long sumRow1 = 0;
+public:
+    long long gridGame(vector<vector<int>>& grid) {
+        const int n = grid[0].size();
+        long ans = LONG_MAX;
+        long sumr0 = accumulate(grid[0].begin(), grid[0].end(), 0L);
+        long sumr2 = 0;
 
-    for (int i = 0; i < n; ++i) {
-      sumRow0 -= grid[0][i];
-      ans = min(ans, max(sumRow0, sumRow1));
-      sumRow1 += grid[1][i];
+        for (int i = 0; i < n; ++i) {
+            sumr0 -= grid[0][i];
+            ans = min(ans, max(sumr0, sumr2));
+            sumr2 += grid[1][i];
+        }
+
+        return ans;
     }
-
-    return ans;
-  }
 };
