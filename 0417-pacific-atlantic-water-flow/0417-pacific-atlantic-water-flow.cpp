59f839683b5a22dbc92b0551719a738843b51ff8
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2025-07-09 17:41:58 +0530

    Time: 17 ms (27.85%), Space: 22.1 MB (89.11%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.cpp
@@ -1,42 +1,35 @@
 class Solution {
- public:
-  vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-    const int m = heights.size();
-    const int n = heights[0].size();
-    vector<vector<int>> ans;
-    vector<vector<bool>> seenP(m, vector<bool>(n));
-    vector<vector<bool>> seenA(m, vector<bool>(n));
+public:
+    void dfs(int r, int c, int n, int m, vector<vector<bool>>& vis,int prevh, vector<vector<int>>& h){
+        if(r<0 || c<0 || r>=n || c>=m || vis[r][c] ||  prevh > h[r][c]) return;
 
-    for (int i = 0; i < m; ++i) {
-      dfs(heights, i, 0, seenP);
-      dfs(heights, i, n - 1, seenA);
+        vis[r][c] = true;
+        dfs(r+1, c, n, m, vis, h[r][c], h);
+        dfs(r, c+1, n, m, vis, h[r][c], h);
+        dfs(r-1, c, n, m, vis, h[r][c], h);
+        dfs(r, c-1, n, m, vis, h[r][c], h);
+        
     }
+    vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
+        int n = heights.size(), m = heights[0].size();
+        vector<vector<bool>> atl(n, vector<bool> (m,0)), pac(n, vector<bool> (m,0));
 
-    for (int j = 0; j < n; ++j) {
-      dfs(heights, 0, j, seenP);
-      dfs(heights, m - 1, j, seenA);
+        for(int i=0;i<n;i++){
+            dfs(i, 0, n, m, pac, heights[i][0], heights);
+            dfs(i, m-1, n, m, atl, heights[i][m-1], heights);
+        }
+        for(int i=0;i<m;i++){
+            dfs(0, i, n, m, pac, heights[0][i], heights);
+            dfs(n-1, i, n, m, atl, heights[n-1][i], heights);
+        }
+
+        vector<vector<int>> ans;
+        for(int r=0;r<n;r++){
+            for(int c=0;c<m;c++){
+                if(pac[r][c] && atl[r][c])
+                    ans.push_back({r,c});
+            }
+        }
+        return ans;
     }
-
-    for (int i = 0; i < m; ++i)
-      for (int j = 0; j < n; ++j)
-        if (seenP[i][j] && seenA[i][j])
-          ans.push_back({i, j});
-
-    return ans;
-  }
-
- private:
-  void dfs(const vector<vector<int>>& heights, int i, int j,
-           vector<vector<bool>>& seen, int h = 0) {
-    if (i < 0 || i == heights.size() || j < 0 || j == heights[0].size())
-      return;
-    if (seen[i][j] || heights[i][j] < h)
-      return;
-
-    seen[i][j] = true;
-    dfs(heights, i + 1, j, seen, heights[i][j]);
-    dfs(heights, i - 1, j, seen, heights[i][j]);
-    dfs(heights, i, j + 1, seen, heights[i][j]);
-    dfs(heights, i, j - 1, seen, heights[i][j]);
-  }
 };
