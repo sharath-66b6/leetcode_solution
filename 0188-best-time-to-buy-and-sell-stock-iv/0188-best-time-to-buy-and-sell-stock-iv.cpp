f42b90939a34e9c205fa14e5e3b6a56831310258
Sharath kumar Reddy <168269094+sharath-66b6@users.noreply.github.com>
2025-12-13 18:16:46 +0530

    Time: 7 ms (52.42%), Space: 17.8 MB (43.86%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,27 +1,43 @@
 class Solution {
 private:
-    vector<vector<vector<int>>> dp;
-    int solve(int i, int buy, vector<int>& p, int k){
-        if(i == p.size() || k == 0) return 0;
-        if(dp[i][buy][k] != -1) return dp[i][buy][k];
+    // vector<ve ctor<vector<int>>> dp;
+    // int solve(int i, int buy, vector<int>& p, int k){
+    //     if(i == p.size() || k == 0) return 0;
+    //     if(dp[i][buy][k] != -1) return dp[i][buy][k];
 
-        if(buy){
-            dp[i][buy][k] = max(-p[i] + solve(i+1, !buy, p, k),
-                                solve(i+1, buy, p, k));
+    //     if(buy){
+    //         dp[i][buy][k] = max(-p[i] + solve(i+1, !buy, p, k),
+    //                             solve(i+1, buy, p, k));
+    //     }
+    //     else{
+    //         dp[i][buy][k] = max(p[i] + solve(i+1, !buy, p, k-1),
+    //                             solve(i+1, buy, p, k));
+    //     }
+    //     return dp[i][buy][k];
+    // }
+
+    vector<vector<int>> dp;
+    int solve(int i, int t, vector<int>& p, int k){
+        if(i == p.size() || t == 2*k) return 0;
+        if(dp[i][t] != -1) return dp[i][t];
+
+        if(t%2 == 0){
+            dp[i][t] = max(-p[i] + solve(i+1, t+1, p, k), solve(i+1, t, p, k));
         }
         else{
-            dp[i][buy][k] = max(p[i] + solve(i+1, !buy, p, k-1),
-                                solve(i+1, buy, p, k));
+            dp[i][t] = max(p[i] + solve(i+1, t+1, p, k), solve(i+1, t, p, k));
         }
-        return dp[i][buy][k];
+
+        return dp[i][t];
     }
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
         if(n < 2) return 0;
-        dp.assign(n+1, vector<vector<int>> (2, vector<int> (k+1,-1)));
-        int ans = solve(0, 1, prices, k);
+        // dp.assign(n+1, vector<vector<int>> (2, vector<int> (k+1,-1)));
+        // int ans = solve(0, 1, prices, k);
         
-        return ans;
+        dp.assign(n, vector<int> (2*k,-1));
+        return solve(0, 0, prices, k);
     }
 };
